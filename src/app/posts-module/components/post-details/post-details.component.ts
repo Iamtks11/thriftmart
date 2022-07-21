b8e81ec4e6014565b1b11acc29cafd3949475893
Iamtks11 <tusharkhorwal11@gmail.com>
2022-07-21 16:05:17 +0530

    minor fix

--- src/app/posts-module/components/post-details/post-details.component.ts
+++ src/app/posts-module/components/post-details/post-details.component.ts
@@ -8,28 +8,26 @@ import { EmptyUserCardData } from '../../../../assets/models/user';
 @Component({
   selector: 'app-post-details',
   templateUrl: './post-details.component.html',
-  styleUrls: ['./post-details.component.css']
+  styleUrls: ['./post-details.component.css'],
 })
 export class PostDetailsComponent implements OnInit {
-
-  constructor(private _data: DataService, private _route: ActivatedRoute, private _router: Router) {
+  constructor(
+    private _data: DataService,
+    private _route: ActivatedRoute,
+    private _router: Router
+  ) {
     this.phone = NaN;
     let query = this._route.snapshot.paramMap.get('id');
-    if(query)
-    {
-      let postId = Number(query);
-      this.post = this._data.getPostById(postId);
-      this.postUser = this._data.getUserCardById(this.post.userId);
-    }
-    else{
-      this._router.navigate(['']);
+
+    let postId = Number(query);
+    this.post = this._data.getPostById(postId);
+    this.postUser = this._data.getUserCardById(this.post.userId);
+    if (this.post.id == 0) {
+      this._router.navigate(['/']);
     }
   }
 
-  ngOnInit(): void {
-  }
-
-
+  ngOnInit(): void {}
 
   post: Post = EmptyPost;
   postUser: userCardData = EmptyUserCardData;
@@ -37,5 +35,4 @@ export class PostDetailsComponent implements OnInit {
   email: string = '';
   phone: number;
   message: string = '';
-
 }
